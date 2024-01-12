a52f1dbd2c2f543969ab85e6aced1eb52b6eb333
snowray712000 <snowray712000@gmail.com>
2024-01-12 09:22:38 +0800

    # 新增 other 資料夾，並以它為主。

--- _231219a.py
+++ _231219a.py
@@ -4,7 +4,9 @@ import open3d as o3d
 from Easy import TpO3d
 import typing as t
 import numpy.typing as npt
-
+import linque as lq
+import os
+import glob
 #%%
 from enumSrcDataLObjSorted import enumSrcDataLObjSorted3
 from Easy.ColorsForCrown import labels_to_colors_crown
@@ -17,7 +19,7 @@ class AppGlobals:
     pathlobj: str
     '''開啟的 pathlobj '''
     upper_lower: str
-    '''開啟的 pathlobj ，是 upper 還是 lower '''
+    '''開啟的 pathlobj ，是 upper 還是 lower ， 還是 other '''
     idd: int
     ver: int
     '''開啟的 id '''
@@ -42,9 +44,26 @@ class AppGlobals:
     radius: float = 0.4
     count_recursive: int = 1 # knn 方法，遞迴的次數
     setValue: np.int8 = 1 
+    dirFileDialog: str = os.getcwd()
+    def enumFiles()->t.List[str]:
+        """ 產生給  ListView 用的資料 """
+        r1 = glob.glob(AppGlobals.dirFileDialog + '/*', recursive=False)
+        r2 = lq.linque(r1).where(lambda a1: os.path.isdir(a1)).select(lambda a1: os.path.basename(a1)).to_list()
+        # print(r2)
+
+        # exts = ['.stl', '.lobj', '.xyz']
+        # exts = ['.stl']
+        exts = ['.lobj']
+        r3 = lq.linque(r1).where(lambda a1: os.path.isfile(a1)).select(lambda a1: os.path.basename(a1)).where(lambda a1: os.path.splitext(a1)[1] in exts).to_list()
+        # print(r3)
+        
+        items = ['..'] + r2 + r3
+        return items
+    """ other list 清單的 """
     ''' 右鍵點擊時，要設定的 label 值 '''
     data_undo: t.List[npt.NDArray[np.int8]] = []
     data_redo: t.List[npt.NDArray[np.int8]] = []
+    isDragPick: bool = False # 右鍵點擊時，是否拖曳，決定 undo redo 用
 
 #%%
 # undo redo 
@@ -153,15 +172,27 @@ for i in range(17):
     else:
         hstack2.add_child(btnColorLabels[i])    
 
+# list3
+vertCollepse3 = TpO3d.CollapsableVert("other", 0, TpO3d.Margins.lazyMarginsEM(winSettings,0.5,0,0.5,0))
+vert1.add_child(vertCollepse3)
+vertCollepse3.set_is_open(True)
+
+list3 = TpO3d.ListView()
+vertCollepse3.add_child(list3)
+list3.set_items(AppGlobals.enumFiles())
+list3.set_max_visible_items(10)
+
+# list1
 vertCollepse1 = TpO3d.CollapsableVert("upper", 0, TpO3d.Margins.lazyMarginsEM(winSettings,0.5,0,0.5,0))
 vert1.add_child(vertCollepse1)
-vertCollepse1.set_is_open(True)
+vertCollepse1.set_is_open(False)
 
 list1 = TpO3d.ListView()
 vertCollepse1.add_child(list1)
 list1.set_max_visible_items(10)
 list1.set_items([f"id_{str(a1[1])}_ver_{str(a1[2])}" for a1 in path_idds_upper])
 
+# list2
 vertCollepse2 = TpO3d.CollapsableVert("lower", 0, TpO3d.Margins.lazyMarginsEM(winSettings,0.5,0,0.5,0))
 vert1.add_child(vertCollepse2)
 vertCollepse2.set_is_open(False)
@@ -169,6 +200,7 @@ vertCollepse2.set_is_open(False)
 list2 = TpO3d.ListView()
 vertCollepse2.add_child(list2)
 list2.set_items([f"id_{str(a1[1])}_ver_{str(a1[2])}" for a1 in path_idds_lower])
+list2.set_max_visible_items(10)
 
 # win3D
 win3D = app.create_window("3D", 768, 768)
@@ -196,20 +228,31 @@ def fn_clickHelp():
     winSettings.show_message_box("Help", "選取檔案：雙擊清單。\n滑鼠左鍵，旋轉。\nCtrl+左滑鼠鍵，平移。\n滑鼠滾輪，縮放。\n右滑鼠鍵，設定 label。\nZ，undo。Y，redo。\n")
 btnHelp.set_on_clicked(fn_clickHelp)
 
+def enum_and_set_list3():
+    """ 產生給  ListView 用的資料 
+    - 使用 AppGlobals.dirFileDialog 路徑資料。
+    """
+    list3.set_items(AppGlobals.enumFiles())
+    
 def reload_and_update_listboxview():
     global path_idds_upper, path_idds_lower
-    path_idds_upper = enumSrcDataLObjSorted3(upperlower="upper")
-    path_idds_lower = enumSrcDataLObjSorted3(upperlower="lower")
-    list1.set_items([f"id_{str(a1[1])}_ver_{str(a1[2])}" for a1 in path_idds_upper])
-    list2.set_items([f"id_{str(a1[1])}_ver_{str(a1[2])}" for a1 in path_idds_lower])
-    
-    def find_index():
-        if AppGlobals.upper_lower == "upper":
-            return next((i for i, x in enumerate(path_idds_upper) if x[1] == AppGlobals.idd and x[2] == AppGlobals.ver), None)
-        else:
-            return next((i for i, x in enumerate(path_idds_lower) if x[1] == AppGlobals.idd and x[2] == AppGlobals.ver), None)
-            
-    list1.selected_index = find_index()
+    if AppGlobals.upper_lower == "upper":
+        path_idds_upper = enumSrcDataLObjSorted3(upperlower="upper")
+        list1.set_items([f"id_{str(a1[1])}_ver_{str(a1[2])}" for a1 in path_idds_upper])
+        list1.selected_index = next((i for i, x in enumerate(path_idds_upper) if x[1] == AppGlobals.idd and x[2] == AppGlobals.ver), None)        
+    elif AppGlobals.upper_lower == "lower":
+        path_idds_lower = enumSrcDataLObjSorted3(upperlower="lower")
+        list2.set_items([f"id_{str(a1[1])}_ver_{str(a1[2])}" for a1 in path_idds_lower])    
+        list2.selected_index = next((i for i, x in enumerate(path_idds_lower) if x[1] == AppGlobals.idd and x[2] == AppGlobals.ver), None)
+    elif AppGlobals.upper_lower == "other":
+        basename = os.path.basename(AppGlobals.pathlobj)
+        names = AppGlobals.enumFiles()
+        list3.set_items(names)
+        
+        def fn_first1(a1: t.Tuple[int,str]):
+            return a1 == basename
+        re: t.Tuple[int,str] = lq.linque( enumerate(names) ).first(fn_first1)
+        list3.selected_index = re[0]
     
 def generate_filename(path, dir):
     base_name = os.path.basename(path)  # 從路徑中提取檔名    
@@ -231,6 +274,7 @@ def fn_btnSaveAs_clicked():
         dir = os.path.dirname(abs_path)    
         path2 = generate_filename(AppGlobals.pathlobj, dir + '/')
         LabeledObject().saveVsLsTris(dir + '/' + path2, AppGlobals.lobj[0], AppGlobals.lobj[3], AppGlobals.lobj[5])
+        
         reload_and_update_listboxview()
         
     except Exception as e:
@@ -377,9 +421,28 @@ def fn_listbox_changed1(str:str, isDclick:bool):
     
 def fn_listbox_changed2(str:str, isDclick):
     _listbox_changed_core(str, isDclick, path_idds_lower, "lower")
+
+
     
+def fn_list3_selected_changed(newvalue, isdb):
+    if isdb:
+        if newvalue == '..':
+            AppGlobals.dirFileDialog = os.path.dirname(AppGlobals.dirFileDialog)
+            enum_and_set_list3()
+        else:
+            path = AppGlobals.dirFileDialog + '/' + newvalue
+            if os.path.isdir(path):
+                AppGlobals.dirFileDialog = path
+                enum_and_set_list3()
+            else:
+                print('file')
+                AppGlobals.upper_lower = "other"                
+                AppGlobals.pathlobj = path
+                openLObj(path)
+        
 list1.set_on_selection_changed(fn_listbox_changed1)
 list2.set_on_selection_changed(fn_listbox_changed2)
+list3.set_on_selection_changed(fn_list3_selected_changed)    
 
 
 def get_adjacencys(idxSeed: int, count_recursive: int, dict_adjacency: t.Dict[int, t.List[int]], already_search = None) -> t.List[int]:
@@ -404,37 +467,71 @@ def get_adjacencys(idxSeed: int, count_recursive: int, dict_adjacency: t.Dict[in
 
 # 右鍵點擊，設定 label，並更新顏色
 def fn_mouse_event(event: TpO3d.MouseEvent)->TpO3d.Widget.EventCallbackResult:
-    if event.type == TpO3d.MouseEvent.Type.BUTTON_DOWN and event.is_button_down(TpO3d.MouseButton.RIGHT):
-        def fn_img(depth_image: TpO3d.Image):
-            img = np.array(depth_image)
-            u = event.x - scene.frame.x
-            v = event.y - scene.frame.y
-            z = img[v][u]
+    if event.type == TpO3d.MouseEvent.Type.WHEEL:
+        # 滾輪
+        if event.is_modifier_down(TpO3d.KeyModifier.SHIFT):            
+            if event.wheel_dy > 0:
+                AppGlobals.count_recursive += 1
+            elif event.wheel_dy < 0:
+                AppGlobals.count_recursive -= 1
             
-            if z == 1.0: # 沒找到
-                print('no click anything u', u, ' v', v)
-            else:                
+            if AppGlobals.count_recursive < 1:
+                AppGlobals.count_recursive = 1
+            elif AppGlobals.count_recursive > sliderCountRecursive.get_maximum_value:
+                AppGlobals.count_recursive = sliderCountRecursive.get_maximum_value
+            
+            sliderCountRecursive.int_value = int(AppGlobals.count_recursive)
+            app.post_to_main_thread(winSettings, lambda: winSettings.post_redraw())
+            return TpO3d.Widget.EventCallbackResult.CONSUMED        
+        elif event.is_modifier_down(TpO3d.KeyModifier.CTRL):
+            if event.wheel_dy > 0:
+                AppGlobals.setValue += 1
+            elif event.wheel_dy < 0:
+                AppGlobals.setValue -= 1
+            
+            if AppGlobals.setValue < 0:
+                AppGlobals.setValue = 0
+            elif AppGlobals.setValue > 16:
+                AppGlobals.setValue = 16
+                
+            sliderSetValue.int_value = int(AppGlobals.setValue)
+            app.post_to_main_thread(winSettings, lambda: winSettings.post_redraw())
+            return TpO3d.Widget.EventCallbackResult.CONSUMED
+            
+    def fn_img(depth_image: TpO3d.Image):
+        img = np.array(depth_image)
+        u = event.x - scene.frame.x
+        v = event.y - scene.frame.y
+        z = img[v][u]
+        
+        if z == 1.0: # 沒找到
+            print('no click anything u', u, ' v', v)
+        else:
+            if AppGlobals.isDragPick == False:                
                 UnDoReDo.push_to_undo_and_clear_redo(AppGlobals.lobj[3])
-                
-                pt = scene3d.camera.unproject(u, v, z, scene.frame.width, scene.frame.height)
-                print(f'u {u} v {v} z {z} pt:{pt[0]} {pt[1]} {pt[2]}')
-                
-                
-                # method 1
-                if AppGlobals.tpMethod == 0:
-                    kdtree = AppGlobals.kdtree                    
-                    radius = AppGlobals.radius                                    
-                    re = kdtree.search_radius_vector_3d(pt, radius)                                       
-                    update_labels_and_colors(re[1])
-                elif AppGlobals.tpMethod == 1:
-                    kdtree = AppGlobals.kdtree
-                    re = kdtree.search_knn_vector_3d(pt, 1)
-                    idxs = get_adjacencys(re[1][0], AppGlobals.count_recursive, AppGlobals.dict_adjacency)                
-                    update_labels_and_colors(idxs)
-    
-            pass            
+            
+            pt = scene3d.camera.unproject(u, v, z, scene.frame.width, scene.frame.height)
+            print(f'u {u} v {v} z {z} pt:{pt[0]} {pt[1]} {pt[2]}')
+                        
+            # method 1
+            if AppGlobals.tpMethod == 0:
+                kdtree = AppGlobals.kdtree                    
+                radius = AppGlobals.radius                                    
+                re = kdtree.search_radius_vector_3d(pt, radius)                                       
+                update_labels_and_colors(re[1])
+            elif AppGlobals.tpMethod == 1:
+                kdtree = AppGlobals.kdtree
+                re = kdtree.search_knn_vector_3d(pt, 1)
+                idxs = get_adjacencys(re[1][0], AppGlobals.count_recursive, AppGlobals.dict_adjacency)                
+                update_labels_and_colors(idxs)        
+    if event.type == TpO3d.MouseEvent.Type.BUTTON_DOWN and event.is_button_down(TpO3d.MouseButton.RIGHT):
+        AppGlobals.isDragPick = False
         scene3d.scene.render_to_depth_image(fn_img)        
         return TpO3d.Widget.EventCallbackResult.CONSUMED
+    if event.type == TpO3d.MouseEvent.Type.DRAG and event.is_button_down(TpO3d.MouseButton.RIGHT):
+        AppGlobals.isDragPick = True
+        scene3d.scene.render_to_depth_image(fn_img)     
+        return TpO3d.Widget.EventCallbackResult.CONSUMED        
     return TpO3d.Widget.EventCallbackResult.IGNORED
 scene.set_on_mouse(fn_mouse_event)
 
